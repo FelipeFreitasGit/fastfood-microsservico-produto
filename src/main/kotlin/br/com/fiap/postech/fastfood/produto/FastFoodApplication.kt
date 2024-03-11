3e46f7e9494509cf325f03d9cf9526e18b8adcdc
FelipeFreitasGit <vasconcellosdefreitas@outlook.com>
2024-03-11 00:40:46 -0300

    ajustando credenciais

--- src/main/kotlin/br/com/fiap/postech/fastfood/produto/FastFoodApplication.kt
+++ src/main/kotlin/br/com/fiap/postech/fastfood/produto/FastFoodApplication.kt
@@ -2,11 +2,12 @@ package br.com.fiap.postech.fastfood.produto
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
 @ComponentScan(basePackages = arrayOf("br.com.fiap.postech.fastfood.produto*"))
 @EnableJpaRepositories("br.com.fiap.postech.fastfood.produto*")
 @EntityScan("br.com.fiap.postech.fastfood.produto*")
--- src/main/kotlin/br/com/fiap/postech/fastfood/produto/config/SecurityConfig.kt
+++ src/main/kotlin/br/com/fiap/postech/fastfood/produto/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package br.com.fiap.postech.fastfood.produto.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
@@ -15,9 +16,13 @@ class SecurityConfig {
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             authorizeRequests {
-                authorize("/**", permitAll)
+                authorize(HttpMethod.POST,"/produto", permitAll)
+                authorize(HttpMethod.PUT,"/produto/**", permitAll)
+                authorize(HttpMethod.DELETE,"/produto/**", permitAll)
+                authorize(HttpMethod.GET, "/produto/**", permitAll)
             }
         }
+        http.csrf { it.disable() }
         return http.build()
     }
 }
